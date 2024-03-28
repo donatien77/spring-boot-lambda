1a81940edff9aacb1e47a0f6b4554e1cc7c19ee8
donatien <macosmonterey@macOSs-MBP.hitronhub.home>
2024-03-27 21:55:55 -0400

    first commit

---
+++ src/main/java/org/donatien/Application.java
@@ -0,0 +1,19 @@
+package org.donatien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import org.donatien.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "org.example.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
